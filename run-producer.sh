8a54bf0994cdafad05721f7e3e611dfb3b32773f
Martin Sauve <msauve@redhat.com>
2021-08-09 12:35:00 -0400

    added kafka crd

---
+++ run-producer.sh
@@ -0,0 +1,1 @@
+kamel run --configmap=appconfig --open-api api/openapi.yaml -t 3scale.enabled=true -t 3scale.auto=true ./integrations/KafkaProducer.java
--- run-provider.sh
+++
@@ -1,1 +0,0 @@
-kamel run --configmap=appconfig --open-api api/openapi.yaml ./integrations/KafkaProducer.java
